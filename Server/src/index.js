80c90d4eb81a6fe3e79e34a0a361a98baf5dccd4
Joseballer <joseballera@gmail.com>
2023-06-24 22:45:29 -0400

    server creado

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,24 @@
-const http = require ('http')
+const fs = require("fs");
+const http = require("http");
+const data = require("./utils/data");
+const { log } = require("console");
 
-http.createServer ((req, res)=>{
+const PORT = 3002;
+http
+  .createServer((req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    const { url } = req;
+    if (url.includes("/rickandmorty/character")) {
+      const id = url.split("/").at(-1);
+      const character = data.find((character) => character.id == id);
 
-    
-});listen (3001, 'localhost')
+      if (character) {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        return res.write(JSON.stringify(character));
+      } else {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        return res.write(JSON.stringify({ message: "Character not found" }));
+      }
+    }
+  })
+  .listen(PORT, "localhost");
